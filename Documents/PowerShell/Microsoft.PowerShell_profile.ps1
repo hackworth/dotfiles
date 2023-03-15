89ab15255ad222174f6412ca6c7ad79613f8ed9f
Jordan Hackworth <jordan.hackworth@das.oregon.gov>
2023-03-15 10:49:19 -0700

    Add powershell profile

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,26 @@
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
+
+# Configuring Oh-my-posh Terminal
+oh-my-posh init pwsh --config "$(scoop prefix oh-my-posh)\themes\amro.omp.json" | Invoke-Expression
+
+# Importing Terminal-Icons Module
+Import-Module Terminal-Icons
+
+# Importing Z Module
+Import-Module Z
+
+# Importing Fuzzy finder tool
+Import-Module PSFzf
+
+# Configuring Fuzzy Finder
+Set-PSReadLineOption -PredictionSource History
+Set-PsFzfOption -PSReadLineChordProvider 'Ctrl+f' -PSreadLineChordReverseHistory 'Ctrl+r'
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Setting Alias's
+Set-Alias vim nvim
+Set-Alias g git
+Set-Alias grep Find-String
